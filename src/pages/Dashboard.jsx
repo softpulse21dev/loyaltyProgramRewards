07cb146e72d0a66b53bf0baf637306d7a43005c9
softpulse21dev <softpulsedev21@gmail.com>
2026-03-09 11:36:07 +0530

    chagnes position of setup guide in dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -300,6 +300,14 @@ const Dashboard = () => {
                                 />
                             </MediaCard> */}
 
+                            {/* Setup Guide */}
+                            {showGuide && hideGuide !== 'true' && (
+                                <SetupGuide
+                                    onDismiss={() => { setShowGuide(false), localStorage.setItem('hideGuide', true) }}
+                                    items={items}
+                                />
+                            )}
+
                             {/* Revenue Section */}
                             <BlockStack gap="300">
                                 <Text as="h1" variant="headingMd">Revenue from Loyalty Programs</Text>
@@ -424,14 +432,6 @@ const Dashboard = () => {
                                     </Layout.Section>
                                 </Layout>
                             </BlockStack>
-
-                            {/* Setup Guide */}
-                            {showGuide && hideGuide !== 'true' && (
-                                <SetupGuide
-                                    onDismiss={() => { setShowGuide(false), localStorage.setItem('hideGuide', true) }}
-                                    items={items}
-                                />
-                            )}
 
                             {/* Collapsible Section */}
                             <Card >
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
     hmr: {
-      host: "relay-belief-merchant-asn.trycloudflare.com",
+      host: "lenders-under-functionality-simon.trycloudflare.com",
       protocol: "wss",
     },
   },
