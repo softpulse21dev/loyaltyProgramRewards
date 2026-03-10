2a56b1df33cba923912a43bcd076d67936d48d30
softpulse21dev <softpulsedev21@gmail.com>
2026-03-10 15:38:29 +0530

    tierview is working now in local

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
     hmr: {
-      host: "relay-belief-merchant-asn.trycloudflare.com",
+      host: "hundred-tigers-montreal-stages.trycloudflare.com",
       protocol: "wss",
     },
   },
